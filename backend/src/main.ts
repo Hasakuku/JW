6046120b9360ce55c62dc04f8f7ede2e1ea63b82
wonsik <seodnjstlr@gmail.com>
2024-02-06 13:43:20 +0900

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,8 +10,8 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    credentials: true,
+    // origin: '*',
+    // credentials: true,
   });
   app.useWebSocketAdapter(new WsAdapter(app));
   app.setGlobalPrefix('api/v1');
