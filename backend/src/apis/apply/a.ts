b5383e3b1638fbc18224d8293e6ad42e76280298
wonsik <seodnjstlr@gmail.com>
2024-02-06 12:44:02 +0900

    feature: apply&meeting entity

--- backend/src/apis/apply/a.ts
+++ backend/src/apis/apply/a.ts
@@ -17,15 +17,13 @@ export class ApplicationStatusSubscriber
     const apply: Apply = event.entity;
 
     if (apply.status === ApplyStatus.ACCEPTED) {
-      const meetingRepository = event.manager.getRepository('meeting');
-      const meeting = await meetingRepository.findOneBy(apply.meetingId, {
-        relations: ['participants'],
-      });
-
-      if (meeting) {
-        meeting.participants.push(apply.userId);
-        await meetingRepository.save(meeting);
-      }
+      // const meetingRepository = event.manager.getRepository('meeting');
+      // const meeting = await meetingRepository.findOneBy(apply.meetingId);
+      // relations: ['participants'],})
+      // if (meeting) {
+      // meeting.participants.push(apply.userId);
+      // await meetingRepository.save(meeting);
+      // }
     }
   }
 }
