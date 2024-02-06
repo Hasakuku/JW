a1340aa3855c4c4f94f5da5c447b517cbc11120e
wonsik <seodnjstlr@gmail.com>
2024-02-07 05:29:52 +0900

    test

--- backend/src/apis/auth/auth.controller.ts
+++ backend/src/apis/auth/auth.controller.ts
@@ -148,7 +148,7 @@ export class AuthController {
       // secure: true,
     });
     // res.setHeader('Authorization', `Bearer ${accessToken}`);
-    res.redirect(process.env.FRONT_URI);
+    // res.redirect(process.env.FRONT_URI);
     // return { message: authMessage.LOGIN_SUCCESS };
   }
 
