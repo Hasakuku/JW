41a4f85446a4447aaa4f11518fa46c3882bce1bc
wonsik <seodnjstlr@gmail.com>
2024-02-08 13:04:59 +0900

    test

--- backend/src/apis/auth/auth.controller.ts
+++ backend/src/apis/auth/auth.controller.ts
@@ -140,7 +140,7 @@ export class AuthController {
   ) {
     const accessToken = await this.authService.login(authCredentialDto);
     res.cookie('jwt', accessToken, { httpOnly: true });
-    res.header('Access-Control-Allow-Origin', 'http://52.62.68.140');
+    res.header('Access-Control-Allow-Origin', 'http://52.62.68.140/');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
